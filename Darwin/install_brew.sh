664cce9ae5079087ebb36ddc1c986796820eff43
Tim Cosgrove <tim@timcosgrove.net>
2016-03-25 13:05:08 -0700

    install cask after homebrew. check if we need to install xcode and clone the repo and only do it as necessary

--- Darwin/install_brew.sh
+++ Darwin/install_brew.sh
@@ -6,3 +6,11 @@ else
   echo "Homebrew not installed. Installing..."
   yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi
+
+which cask &> /dev/null
+if [ $? -eq 0 ]; then
+  echo "Cask already installed."
+else
+  echo "Cask not installed. Installing..."
+  brew install cask
+fi
--- setup.sh
+++ setup.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-  # Apple installs basic dev commands but doesn't let you use them without going through UI-based dialogs. Let's bypass that.
-  xcode-select -p &> /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Xcode commandline tools already installed."
-  else
-    echo "Xcode commandline tools not installed. Installing..."
-    PROD=$(softwareupdate -l |
-    grep "\*.*Command Line" |
-    head -n 1 | awk -F"*" '{print $2}' |
-    sed -e 's/^ *//' |
-    tr -d '\n')
-    softwareupdate -i "$PROD" -v;
+	# Let's make the possibly naive assumption that the absence of ./Darwin/setup.sh means we need to install xcode and clone the repo
+	# If it's not there, do that.
+	if [ ! -e ./Darwin/setup.sh ]; then
+    # Apple installs basic dev commands but doesn't let you use them without going through UI-based dialogs. Let's bypass that.
+    xcode-select -p &> /dev/null
+    if [ $? -eq 0 ]; then
+      echo "Xcode commandline tools already installed."
+    else
+      echo "Xcode commandline tools not installed. Installing..."
+      PROD=$(softwareupdate -l |
+          grep "\*.*Command Line" |
+          head -n 1 | awk -F"*" '{print $2}' |
+          sed -e 's/^ *//' |
+          tr -d '\n')
+      softwareupdate -i "$PROD" -v;
+    fi
+    # Now that we actually have tools, download this setup repo and carry on.
+    git clone https://github.com/timcosgrove/.setup
+    cd .setup
   fi
-  # Now that we actually have tools, download this setup repo and carry on.
-  git clone https://github.com/timcosgrove/.setup
-  .setup/Darwin/setup.sh
+	./Darwin/setup.sh
 fi
