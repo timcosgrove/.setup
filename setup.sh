859f848012211bf318d1268ea23ab47d676d12a3
Tim Cosgrove <tcosgrove@twitter.com>
2016-03-25 12:28:25 -0700

    attempt to setup setup.sh to install xcode tools when run via wget so that we can then bootstrap the rest of the setup process

--- setup.sh
+++ setup.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" == "Darwin" ]; then
-  # For time being, do this kind of stupid.
-  ./Darwin/setup.sh
+  # Apple installs basic dev commands but doesn't let you use them without going through UI-based dialogs. Let's bypass that.
+  xcode-select -p &> /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Xcode commandline tools already installed."
+  else
+    echo "Xcode commandline tools not installed. Installing..."
+    PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+    softwareupdate -i "$PROD" -v;
+  fi
+  # Now that we actually have tools, download this setup repo and carry on.
+  git clone https://github.com/timcosgrove/.setup
+  .setup/Darwin/setup.sh
 fi
